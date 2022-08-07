3c19d41282bf418eb39e4767a8c3fd7566de0f89
Vinay Varma <vin1990@gmail.com>
2022-08-08 01:04:26 +0800

    updated

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
+#!/bin/bash -ex
 DOTFILE_PATH=`pwd`
-ln -s $DOTFILE_PATH/dot-profile ~/.profile 
-ln -s $DOTFILE_PATH/dot-vimrc ~/.vimrc
-ln -s $DOTFILE_PATH/dot-vim/ ~/.vim
-ln -s $DOTFILE_PATH/dot-config/ ~/.config
-ln -s $DOTFILE_PATH/dot-tmux-powerlinerc ~/.tmux-powerlinerc 
-ln -s $DOTFILE_PATH/dot-tmux.conf ~/.tmux.conf
-ln -s $DOTFILE_PATH/tmuxifier ~/.tmuxifier
+
+create_symlink(){
+    src=$1
+    dest=$2
+    if [ -e "$dest" ]; then
+        echo "$dest exists. Skipping."
+    else
+        ln -s $src $dest
+    fi
+}
+create_symlink $DOTFILE_PATH/dot-profile ~/.profile
+create_symlink $DOTFILE_PATH/dot-vimrc ~/.vimrc
+create_symlink $DOTFILE_PATH/dot-vim/ ~/.vim
+create_symlink $DOTFILE_PATH/dot-tmux-powerlinerc ~/.tmux-powerlinerc 
+create_symlink $DOTFILE_PATH/dot-tmux.conf ~/.tmux.conf
+create_symlink $DOTFILE_PATH/tmuxifier ~/.tmuxifier
+create_symlink $DOTFILE_PATH/dot-tmux ~/.tmux
