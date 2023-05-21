64e34479889d4d15ecc0a7eb4b6f7c5d336344a4
Vinay Varma <varmavinaym@gmail.com>
2023-05-21 11:27:07 +0800

    feat: add codestats

--- nvim/lua/nvr/packer.lua
+++ nvim/lua/nvr/packer.lua
@@ -43,27 +43,35 @@ return require('packer').startup(function(use)
             }
         end
     }
-    use ('vimwiki/vimwiki')
+    use('vimwiki/vimwiki')
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },  -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },       -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },   -- Required
-            { 'hrsh7th/cmp-buffer' },     -- Optional
-            { 'hrsh7th/cmp-path' },       -- Optional
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
             { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' },   -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },           -- Required
+            { 'L3MON4D3/LuaSnip' },             -- Required
             { 'rafamadriz/friendly-snippets' }, -- Optional
         }
+    }
+    use {
+        'YannickFricke/codestats.nvim',
+        rocks = "lunajson",
+        config = function()
+            require('codestats-nvim').setup()
+        end,
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 end)
