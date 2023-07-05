e0094464ae688671e957b11cce5bc27429ab35be
Vinay Varma <vinay.varma@transcelestial.com>
2023-07-05 23:06:25 +0800

    fix: update tmux config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 DOTFILE_PATH=$(pwd)
 if [ ! -d ~/.oh-my-zsh ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh -s -- -y
 fi
 if [ ! -d ~/.zplug/ ]; then
     curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
@@ -14,6 +14,10 @@ mkdir -p ~/.config
 create_symlink(){
     src=$1
     dest=$2
+    force=$3
+    if [ -e "$dest" ] && [ -n "$force" ]; then
+      rm "$dest"
+    fi
     if [ -e "$dest" ]; then
         echo "$dest exists. Skipping."
     else
@@ -21,8 +25,8 @@ create_symlink(){
         ln -s "$src" "$dest"
     fi
 }
-create_symlink "$DOTFILE_PATH"/dot-profile ~/.profile
-create_symlink "$DOTFILE_PATH"/dot-tmux.conf ~/.tmux.conf
+create_symlink "$DOTFILE_PATH"/dot-profile ~/.profile FORCE
+create_symlink "$DOTFILE_PATH"/dot-tmux.conf ~/.tmux.conf FORCE 
 create_symlink "$DOTFILE_PATH"/dot-tmux ~/.tmux
-create_symlink "$DOTFILE_PATH/dot-config" ~/.config/nvim
-create_symlink "$DOTFILE_PATH/dot-zshrc" ~/.zshrc
+create_symlink "$DOTFILE_PATH/dot-config/nvim" ~/.config/nvim
+create_symlink "$DOTFILE_PATH/dot-zshrc" ~/.zshrc FORCE
