7ca0c9de88494a436792fa71afb5999efb204ec1
Vinay Varma <vinay.varma@transcelestial.com>
2024-01-15 11:13:04 +0800

    updates: 15 jan

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,9 @@
 return {
   "neovim/nvim-lspconfig",
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { 'gd', vim.lsp.buf.definition }
+  end,
   opts = {
     autoformat = false,
     servers = {
