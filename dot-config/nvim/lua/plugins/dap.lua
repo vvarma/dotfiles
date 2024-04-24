e90b60000d47545861c2f128d5cc991ab97a1918
Vinay Varma <varmavinaym@gmail.com>
2024-04-24 22:12:07 +0800

    feat: rustacean

---
+++ dot-config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,30 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+    dap.adapters.lldb = {
+      type = "server",
+      host = "127.0.0.1",
+      port = 13000,
+      executable = {
+        command = "/usr/bin/codelldb",
+        args = { "--port", "13000" },
+
+        -- on windows you may have to uncomment this:
+        -- detached = false,
+      },
+    }
+    dap.configurations.rust = {
+      {
+        name = "Launch",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+      },
+    }
+  end,
+}
--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -7,9 +7,55 @@ return {
   opts = {
     autoformat = false,
     servers = {
-      clangd = { mason = false },
+      clangd = {
+        root_dir = function(fname)
+          return require("lspconfig.util").root_pattern(
+            "Makefile",
+            "configure.ac",
+            "configure.in",
+            "config.h.in",
+            "meson.build",
+            "meson_options.txt",
+            "build.ninja"
+          )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+            fname
+          ) or require("lspconfig.util").find_git_ancestor(fname)
+        end,
+        capabilities = {
+          offsetEncoding = { "utf-16" },
+        },
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+        },
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true,
+          clangdFileStatus = true,
+        },
+      },
       bashls = {},
-      rust_analyzer = {},
+      --rust_analyzer = {
+      --  imports = {
+      --    granularity = {
+      --      group = "module",
+      --    },
+      --    prefix = "self",
+      --  },
+      --  cargo = {
+      --    buildScripts = {
+      --      enable = true,
+      --    },
+      --  },
+      --  procMacro = {
+      --    enable = true,
+      --  },
+      --},
       cmake = {
         settings = {
           cmake = {
@@ -20,6 +66,13 @@ return {
         },
       },
       gopls = {},
+    },
+    setup = {
+      clangd = function(_, opts)
+        local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
+        require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+        return false
+      end,
     },
   },
 }
