4e2d5e169dd6cc0c6f88ae40c16455bb1dc60558
Vinay Varma <vinay.varma@transcelestial.com>
2024-08-28 12:55:19 +0800

    updates: 28 aug

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,4 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.opt.clipboard = "unnamedplus"
+
 vim.g.python3_host_prog = "$HOME/.pyenv/versions/nvim/bin/python"
---
+++ dot-config/nvim/lua/plugins/tabby.lua
@@ -0,0 +1,6 @@
+--- lua
+vim.g.tabby_keybinding_accept = "<C-y>"
+return {
+  "TabbyML/vim-tabby",
+  event = "VimEnter",
+}
